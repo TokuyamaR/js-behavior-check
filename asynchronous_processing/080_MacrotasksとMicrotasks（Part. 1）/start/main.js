977fc8c1792d591d6a11aa707bfdb85df68aa615
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-06 10:13:07 +0900

    learn microtask and macrotask

---
+++ asynchronous_processing/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
@@ -0,0 +1,30 @@
+// マクロタスク
+// タスクキューのこと
+// マイクロタスク
+// タスクキューとは別で存在する非同期処理の待ち行列(ジョブキュー)
+
+// 実行優先順序
+// マイクロタスク > マクロタスク
+// ジョブの実行の違い
+// マイクロ : 順番が回ってきたら全てのタスクを実行
+// マクロ : 順番が回ってきたら一つずつタスクを実行(一つ終了した時点でマイクロタスクが追加された場合はそちらに処理が移る)
+// 代表的な関数
+// マイクロ : Promises, queueMicrotask, MutationObserver ...
+// マクロ : setTimeout, setInterval, requestAnimationFrame ...
+
+// ④非同期(マクロタスク)
+setTimeout(function () {
+  console.log("setTimeout");
+});
+
+new Promise(function (resolve) {
+  // ①
+  console.log("promise");
+  resolve();
+}).then(function () {
+  // ③非同期(マイクロタスク)
+  console.log("then");
+});
+
+// ②
+console.log("global end");
