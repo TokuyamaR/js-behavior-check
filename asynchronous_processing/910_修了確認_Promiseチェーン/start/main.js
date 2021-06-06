8c342ba7810b7fa9a4b804a50f34d8329fd8eb4d
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-07 08:59:16 +0900

    promise practice

--- asynchronous_processing/910_修了確認_Promiseチェーン/start/main.js
+++ asynchronous_processing/910_修了確認_Promiseチェーン/start/main.js
@@ -35,3 +35,81 @@ async function myFetch(fileName) {
   const json = await response.json();
   return json;
 }
+
+async function fetchUser1() {
+  try {
+    const user1 = await myFetch("user1.json");
+    console.log(`--${user1.name}'s timeline--`);
+    if (user1.id) {
+      return user1;
+    } else {
+      throw new Error("user1 data not found");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function fetchFriendsIds(userId) {
+  try {
+    const userIds = await myFetch(`friendsOf${userId}.json`);
+    if (!userIds.length) {
+      return userIds;
+    } else {
+      throw new Error("friendsIds data not found");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function fetchUsers(friendIds) {
+  try {
+    // 詰まった点：foreachでawaitを使おうとしたらエラーが出たので繰り返し処理の記述ができなかった
+    // const userIds = friendIds.friendIds;
+    // let user = await myFetch(`user${userIds[0]}.json`);
+    // let message = await myFetch(`message${user.latestMsgId}.json`);
+    // console.log(`${user.name} says: ${message.message}`);
+
+    // user = await myFetch(`user${userIds[1]}.json`);
+    // message = await myFetch(`message${user.latestMsgId}.json`);
+    // console.log(`${user.name} says: ${message.message}`);
+
+    // user = await myFetch(`user${userIds[2]}.json`);
+    // message = await myFetch(`message${user.latestMsgId}.json`);
+    // console.log(`${user.name} says: ${message.message}`);
+
+    // user = await myFetch(`user${userIds[3]}.json`);
+    // message = await myFetch(`message${user.latestMsgId}.json`);
+    // console.log(`${user.name} says: ${message.message}`);
+
+    // リファクタリング
+    // ①Setオブジェクトの活用
+    // ②Promise.all()
+    const userList = new Set();
+    for (const id of friendIds.friendIds) {
+      userList.add(myFetch(`user${id}.json`));
+    }
+    const users = await Promise.all(userList);
+
+    const messagaList = new Set();
+    for (const user of users) {
+      messagaList.add(myFetch(`message${user.latestMsgId}.json`));
+    }
+    const messages = await Promise.all(messagaList);
+
+    for (const user of users) {
+      for (const message of messages) {
+        if (user.id === message.userId) {
+          console.log(`${user.name} says: ${message.message}`);
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+fetchUser1()
+  .then((user1) => fetchFriendsIds(user1.id))
+  .then((userIds) => fetchUsers(userIds));
