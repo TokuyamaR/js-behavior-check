28c106f28529a4b30706b9de3e08ebbea091f233
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-10 19:36:41 +0900

    practice module

--- javascript_moduler/910_修了確認_モジュール化/start/calc.js
+++ javascript_moduler/910_修了確認_モジュール化/start/calc.js
@@ -3,6 +3,9 @@
  * 以下の即時関数をモジュール化してください。
  * ※calcオブジェクトはmain.js内で行い、実行してください。
  */
+
+// main.jsでの呼び出され方を確認し、関数からobjectに変更する
+// valはプライベート変数としたいため、export外に出す
 let val = 0;
 export const calc = {
   plus: function (target) {
