73f567ddf480e2f18b498282d1069173d752b95f
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-13 08:59:03 +0900

    learn array #2

--- various_objects/090_Array（配列）２/start/main.js
+++ various_objects/090_Array（配列）２/start/main.js
@@ -1,1 +1,23 @@
 const arry = [1, 2, 3, 4, 5];
+
+// arry.forEach((v, i, arry) => {
+//   console.log(v);
+//   console.log(i);
+// });
+
+// map　渡ってきた配列を元に新しい配列を作成する
+const newArry = arry.map((v, i, arry) => {
+  console.log(v);
+  return 2 * v;
+});
+
+console.log(arry, newArry);
+
+// filter 渡ってきた配列の要素でtruthyな要素をだけで配列を再生成する
+
+const filterArry = arry.filter((v, i, arry) => {
+  console.log(v);
+  return i >= 1;
+});
+
+console.log(arry, filterArry);
