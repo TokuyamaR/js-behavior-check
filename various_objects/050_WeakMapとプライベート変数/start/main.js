c2d79dc3697dba260bd4210b3a5974d956e9c90e
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-12 21:53:14 +0900

    learn WeakMap and private variables

--- various_objects/050_WeakMapとプライベート変数/start/main.js
+++ various_objects/050_WeakMapとプライベート変数/start/main.js
@@ -1,4 +1,10 @@
-import { Person } from './person.js';
+import { Person } from "./person.js";
 
-const tim = new Person('Tim', 23);
+const tim = new Person("Tim", 23);
+const bob = new Person("Bob", 29);
 tim.hello();
+bob.hello();
+// _nameは外部からアクセスできないプライベート変数とするよう明示されている
+// しかし、現在のままではアクセスできてしまう
+// -> WeakMapで外部からアクセスできないようにする
+console.log(tim._name);
--- various_objects/050_WeakMapとプライベート変数/start/person.js
+++ various_objects/050_WeakMapとプライベート変数/start/person.js
@@ -1,9 +1,19 @@
+// WeakMapを使うメリット
+// ①外部から_nameにアクセスできないようにする
+// ②インスタンス化されたオブジェクトごとに操作が可能(値を取り出すなど)
+const wm = new WeakMap();
 export class Person {
-    constructor(name, age) {
-        this._name = name;
-    }
+  constructor(name, age) {
+    // this._name = name;
 
-    hello() {
-        console.log(`hello ${this._name}`);
-    }
+    // wmはexportされていないため、外部からアクセスできない
+    // (main.jsからの呼び出しはundefinedとなる)
+    wm.set(this, {
+      name,
+    });
+  }
+
+  hello() {
+    console.log(`hello ${wm.get(this).name}`);
+  }
 }
