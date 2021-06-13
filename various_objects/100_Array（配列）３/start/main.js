171ba244c2e65ed644feaef9d9437fc55116e228
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-13 09:05:37 +0900

    learn array #3

--- various_objects/100_Array（配列）３/start/main.js
+++ various_objects/100_Array（配列）３/start/main.js
@@ -1,1 +1,10 @@
 const arry = [1, 2, 3, 4, 5];
+
+// reduce 配列の要素を一つずつ取り出して、単一の出力値を生成する
+// 第２引数で初期値を設定できる
+const reduceArry = arry.reduce((accu, curr) => {
+  console.log(accu, curr);
+  return accu + curr * 2;
+}, 0);
+
+console.log(reduceArry);
