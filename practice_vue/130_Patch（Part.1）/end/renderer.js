04d16d07767c63f32a27d1543a1075641a22242e
TokuyamaR <qpflapper.1007@gmail.com>
2021-06-21 09:09:10 +0900

    learn vue patch #2

--- practice_vue/130_Patch（Part.1）/end/renderer.js
+++ practice_vue/130_Patch（Part.1）/end/renderer.js
@@ -1,21 +1,21 @@
-import { nodeOps } from './nodeOps.js';
+import { nodeOps } from "./nodeOps.js";
 
-function createVNode(type = '', props = {}, children = '') {
+function createVNode(type = "", props = {}, children = "") {
   return {
     type,
     props,
-    children
-  }
+    children,
+  };
 }
 
 function patch(n1, n2, container) {
   console.log(n1, n2);
   let el;
-  if(n1.type !== n2.type) {
+  if (n1.type !== n2.type) {
     el = nodeOps.create(n2.type);
     nodeOps.append(container, el);
   }
-  if(n1.children !== n2.children) {
+  if (n1.children !== n2.children) {
     nodeOps.html(el, n2.children);
   }
 }
